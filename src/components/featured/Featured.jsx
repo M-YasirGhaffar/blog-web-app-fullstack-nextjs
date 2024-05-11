3ad9ff4fb0bac8c9d3420be4538700599a7470ee
M-YasirGhaffar <mghaffar.bese21seecs@seecs.edu.pk>
2024-05-12 01:28:03 +0500

    changes in styles

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -12,7 +12,7 @@ const Featured = () => {
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>
-        <b>Hey, {loading ? "Guest" : session?.user?.name || "Guest"}!</b>{" "}
+        Hey, <b> {loading ? "Guest" : session?.user?.name || "Guest"}!</b>{" "}
         {session ? 'Create, discover and share stories and ideas.' : 'Discover our stories and creative ideas.'}
       </h1>
       {/* <div className={styles.post}>
