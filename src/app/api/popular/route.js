6af0eaa2ea70e0acfd9c2760638f8584c785f32e
M-YasirGhaffar <mghaffar.bese21seecs@seecs.edu.pk>
2024-06-02 01:12:05 +0500

    popular side menu

---
+++ src/app/api/popular/route.js
@@ -0,0 +1,28 @@
+import { getAuthSession } from "@/utils/auth";
+import prisma from "@/utils/connect";
+import { NextResponse } from "next/server";
+
+export const GET = async (req) => {
+  const POST_PER_PAGE = 5; // Change this to the number of posts you want to send
+
+  const query = {
+    take: POST_PER_PAGE,
+    orderBy: { views: 'desc' }, // Order by views in descending order
+  };
+
+  try {
+    const [posts, totalPosts] = await prisma.$transaction([
+      prisma.post.findMany({
+        ...query,
+        include: { user: true }, // Include the related user
+      }),
+      prisma.post.count(), // Count all posts
+    ]);
+    return new NextResponse(JSON.stringify({ posts, totalPosts }, { status: 200 }));
+  } catch (err) {
+    console.log(err);
+    return new NextResponse(
+      JSON.stringify({ message: "Something went wrong!" }, { status: 500 })
+    );
+  }
+};
--- src/components/menuPosts/MenuPosts.jsx
+++ src/components/menuPosts/MenuPosts.jsx
@@ -1,83 +1,45 @@
+"use client"
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./menuPosts.module.css"
 
 const MenuPosts = ({ withImage }) => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await fetch('http://localhost:3000/api/popular');
+      const data = await res.json();
+      setPosts(data.posts);
+    };
+
+    fetchPosts();
+  }, []);
+
   return (
     <div className={styles.items}>
-      <Link href="/" className={styles.item}>
-        {withImage && (
-          <div className={styles.imageContainer}>
-            <Image src="/p1.jpeg" alt="" fill className={styles.image} />
+      {posts.map((post) => (
+        <Link href={`/posts/${post.slug}`} key={post._id} className={styles.item}>
+          {withImage && post.img && (
+            <div className={styles.imageContainer}>
+              <Image src={post.img} alt="" fill className={styles.image} />
+            </div>
+          )}
+          <div className={styles.textContainer}>
+            <span className={`${styles.category} ${styles[post.catSlug]}`}>{post.catSlug}</span>
+            <h3 className={styles.postTitle}>{post.title}</h3>
+            <div className={styles.detail}>
+              <span className={styles.username}>{post.user.name}</span>
+              <span className={styles.date}> - {new Date(post.createdAt).toLocaleDateString("en-US", {
+                year: "numeric",
+                month: "long",
+                day: "numeric",
+              })}</span>
+            </div>
           </div>
-        )}
-        <div className={styles.textContainer}>
-          <span className={`${styles.category} ${styles.travel}`}>Travel</span>
-          <h3 className={styles.postTitle}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          </h3>
-          <div className={styles.detail}>
-            <span className={styles.username}>John Doe</span>
-            <span className={styles.date}> - 10.03.2023</span>
-          </div>
-        </div>
-      </Link>
-      <Link href="/" className={styles.item}>
-        {withImage && (
-          <div className={styles.imageContainer}>
-            <Image src="/p1.jpeg" alt="" fill className={styles.image} />
-          </div>
-        )}
-        <div className={styles.textContainer}>
-          <span className={`${styles.category} ${styles.culture}`}>
-            Culture
-          </span>
-          <h3 className={styles.postTitle}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          </h3>
-          <div className={styles.detail}>
-            <span className={styles.username}>John Doe</span>
-            <span className={styles.date}> - 10.03.2023</span>
-          </div>
-        </div>
-      </Link>
-      <Link href="/" className={styles.item}>
-        {withImage && (
-          <div className={styles.imageContainer}>
-            <Image src="/p1.jpeg" alt="" fill className={styles.image} />
-          </div>
-        )}
-        <div className={styles.textContainer}>
-          <span className={`${styles.category} ${styles.food}`}>Food</span>
-          <h3 className={styles.postTitle}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          </h3>
-          <div className={styles.detail}>
-            <span className={styles.username}>John Doe</span>
-            <span className={styles.date}> - 10.03.2023</span>
-          </div>
-        </div>
-      </Link>
-      <Link href="/" className={styles.item}>
-        {withImage && (
-          <div className={styles.imageContainer}>
-            <Image src="/p1.jpeg" alt="" fill className={styles.image} />
-          </div>
-        )}
-        <div className={styles.textContainer}>
-          <span className={`${styles.category} ${styles.fashion}`}>
-            Fashion
-          </span>
-          <h3 className={styles.postTitle}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          </h3>
-          <div className={styles.detail}>
-            <span className={styles.username}>John Doe</span>
-            <span className={styles.date}> - 10.03.2023</span>
-          </div>
-        </div>
-      </Link>
+        </Link>
+      ))}
     </div>
   );
 };
