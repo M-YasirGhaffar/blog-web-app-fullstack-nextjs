52e02fa02ef2a4cb406848ae5499a8f76f886b41
M-YasirGhaffar <mghaffar.bese21seecs@seecs.edu.pk>
2024-06-04 02:26:54 +0500

    env and base url fix

--- src/app/posts/[slug]/page.jsx
+++ src/app/posts/[slug]/page.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import Comments from "@/components/comments/Comments";
 
 const getData = async (slug) => {
-  const res = await fetch(`${NEXTAUTH_URL}/api/posts/${slug}`, {
+  const res = await fetch(`${process.env.NEXTAUTH_URL}/api/posts/${slug}`, {
     cache: "no-store",
   });
 
